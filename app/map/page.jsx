d272e99c89752c3318c47cc457e9636110416049
Btreecode <104658377+Btreecode@users.noreply.github.com>
2024-10-23 16:43:30 +0000

    map fixed

--- app/map/page.jsx
+++ app/map/page.jsx
@@ -3,6 +3,15 @@
 import GoogleMapReact from "google-map-react";
 import { IoIosPin } from "react-icons/io";
 
+const MARKERS = [
+  { lat: 33.7545573, lng: -117.8706079 },
+  { lat: 33.7519656, lng: -117.8721971 },
+  { lat: 33.7026548, lng: -117.7841254 },
+  { lat: 33.6944381, lng: -117.7772115 },
+  { lat: 33.6684907, lng: -117.7563644 },
+  { lat: 33.7029825, lng: -117.7121083 },
+];
+
 export default function MapPage() {
   const defaultProps = {
     center: {
@@ -12,6 +21,17 @@ export default function MapPage() {
     zoom: 11,
   };
 
+  const renderMarkers = (map, maps) => {
+    return MARKERS.map(
+      (m) =>
+        new maps.Marker({
+          position: m,
+          map,
+          title: "school",
+        })
+    );
+  };
+
   return (
     // Important! Always set the container height explicitly
     <div style={{ height: "100vh", width: "100%" }}>
@@ -19,13 +39,9 @@ export default function MapPage() {
         bootstrapURLKeys={{ key: "AIzaSyABvv2OoDgpkUKitaNNvDTPKrM54nTiU38" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
+        onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
+        yesIWantToUseGoogleMapApiInternals
       >
-        <Marker lat={33.7545573} lng={-117.8706079} />
-<Marker lat={33.7519656} lng={-117.8721971} />
-<Marker lat={33.7026548} lng={-117.7841254} />
-<Marker lat={33.6944381} lng={-117.7772115} />
-<Marker lat={33.6684907} lng={-117.7563644} />
-<Marker lat={33.7029825} lng={-117.7121083} />
       </GoogleMapReact>
     </div>
   );
