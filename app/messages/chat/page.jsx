6b8144c0b8f0b0f2db2625acd070c954729fe64d
Btreecode <104658377+Btreecode@users.noreply.github.com>
2024-10-23 17:43:46 +0000

    messages css

--- app/messages/chat/page.jsx
+++ app/messages/chat/page.jsx
@@ -3,15 +3,25 @@ import Link from "next/link";
 import { useState } from "react";
 import "./style.css";
 import { addMinutes, format, formatDistance } from "date-fns";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
 let dylan_msgs = [
   // alex says Hey Dylan! Are you free to meet up later? To get your evening gown for a wedding this weekend?
-  { name: "Dylan", txt: "Hey! Yeah, I’m free this afternoon. I’m free after 4. Does that work for you?" },
+  {
+    name: "Dylan",
+    txt: "Hey! Yeah, I’m free this afternoon. I’m free after 4. Does that work for you?",
+  },
   //Perfect! How about we meet at that cafe on Main Street at 4:30?
-  { name: "Dylan", txt: "Sounds perfect! Do you need me to bring anything else?" },
+  {
+    name: "Dylan",
+    txt: "Sounds perfect! Do you need me to bring anything else?",
+  },
   //nope, thanks for the help. Do you wanna grab a drink there?
-  { name: "Dylan", txt: "Great! I’ll bring the gown. It’s all cleaned and ready to go." },
-  //Awesome! I can’t wait to see it. 
+  {
+    name: "Dylan",
+    txt: "Great! I’ll bring the gown. It’s all cleaned and ready to go.",
+  },
+  //Awesome! I can’t wait to see it.
   { name: "Dylan", txt: "Awesome! see you at 3 then." },
   //Thanks!
   { name: "Dylan", txt: "No Problem!" },
@@ -48,20 +58,25 @@ export default function Home() {
   }
 
   return (
-    <div className="mx-5 space-y-5">
-      <div className="fixed top-0 left-0 right-0 p-2 z-10 border-2">
-            <div className="flex justify-between text-xl font-bold">
-                <Link href="/messages">{"<"}</Link>
-                <div> Dylan </div>
-                <div></div>
-            </div>
-        </div>
-      <div className="pt-10 pb-5">
+    <>
+      <div className="p-5 flex justify-between text-xl font-bold">
+        <Link href="/messages">
+          <FaChevronLeft />
+        </Link>
+        <div> Dylan </div>
+        <div></div>
+      </div>
+      <div className="p-5">
         {msgs.map((msg, i) => (
           <div key={i}>
             {msg.name === "me" ? (
               <div className="flex justify-end items-end space-x-2">
-                <div className="text-xs text-gray-500">
+                <div
+                  className="text-xs text-gray-500"
+                  style={{
+                    maxWidth: "70%",
+                  }}
+                >
                   {format(msg.time, "h:mm aa")}
                 </div>
                 <div className="flex justify-end box3 rounded-xl rounded-tr-none p-3 pr-5">
@@ -80,12 +95,18 @@ export default function Home() {
                 </div>
                 <div className="space-y-2">
                   <div>{msg.name}</div>
-                  <div className=" flex items-end" >
-                    <div className="box3 gray rounded-xl rounded-tl-none p-3 pl-5">{msg.txt}</div>
+                  <div className=" flex items-end">
+                    <div
+                      className="box3 gray rounded-xl rounded-tl-none p-3 pl-5"
+                      style={{
+                        maxWidth: "70%",
+                      }}
+                    >
+                      {msg.txt}
+                    </div>
                     <div className="text-xs text-gray-500">
-                    {format(msg.time, "h:mm aa")}
-                  </div>
-
+                      {format(msg.time, "h:mm aa")}
+                    </div>
                   </div>
                 </div>
               </div>
@@ -93,12 +114,19 @@ export default function Home() {
           </div>
         ))}
       </div>
-      <div className= "fixed bottom-0 left-0 right-0 p-4 bg-white z-10">
-        <form onSubmit={onSubmit}>
-          <input className="box3 gray rounded-xl p-2 pl-3 pr-3 w-11/12  " name="textInput" />
-          <button> {">"}</button>
-        </form>
-      </div>
-    </div>
+      <div className="flex-1" style={{ minHeight: 20 }}></div>
+      <form
+        onSubmit={onSubmit}
+        className=" p-4 flex justify-between w-full"
+      >
+        <input
+          className="gray rounded-xl bg-gray-200 px-3 py-2 w-3/4"
+          name="textInput"
+        />
+        <button>
+          <FaChevronRight />
+        </button>
+      </form>
+    </>
   );
 }
